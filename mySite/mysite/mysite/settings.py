71d5fbc67a6fe325516af67faeaf2f794078593b
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-06 15:06:27 +0000

    added the help section

--- mySite/mysite/mysite/settings.py
+++ mySite/mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILE_DIRS = [
+    BASE_DIR / "static",
+    'var/www/static',
+]
--- mySite/mysite/mysite/urls.py
+++ mySite/mysite/mysite/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+from mySite.mysite import home
+from mysite.views import home
+
 urlpatterns = [
         path('polls/', include('polls.urls')),
         path('admin/', admin.site.urls),
+        path('', home),
         ]
 
 
