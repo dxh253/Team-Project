7754815875a1904366967ecb0d13f01f91196a79
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-06 20:26:47 +0000

    fixed css

--- mySite/mysite/mysite/settings.py
+++ mySite/mysite/mysite/settings.py
@@ -123,14 +123,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILE_DIRS = [
-    BASE_DIR / "static",
+# STATIC_URL = 'static/'
+# STATICFILE_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static',
+# ]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
     '/var/www/static',
 ]
+
+
+
