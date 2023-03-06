7ff1bd2c9a8570330abba87ad5b26aea8d714b61
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-06 15:59:58 +0000

    fixed

--- mySite/mysite/mysite/urls.py
+++ mySite/mysite/mysite/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from . import views
 # from mySite.mysite import home
-from . import views
 from . views import home
 
 urlpatterns = [
