fd2da86620e639a698fc830934dd7d12c8bd30a1
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-06 15:28:20 +0000

    fixed

--- mySite/mysite/mysite/urls.py
+++ mySite/mysite/mysite/urls.py
@@ -4,11 +4,13 @@ from django.urls import include, path
 from . import views
 # from mySite.mysite import home
 from . import views
+from . views import home
 
 urlpatterns = [
         path('polls/', include('polls.urls')),
         path('admin/', admin.site.urls),
-        path('', views.home()),
+        path('', home),
+        # path('', views.home()),
         ]
 
 
