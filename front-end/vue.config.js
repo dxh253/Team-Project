e0754ee7062cc2e54563c6453fb5ec15e288367e
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-13 19:08:14 +0000

    fixed cors hearder not working for vm

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -7,12 +7,15 @@ module.exports = {
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-// devServer: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://127.0.0.1:8000',
-//         changeOrigin: true
-//       }
-//     }
-//   }
+devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^api': '/api/v1'
+        }
+      }
+    }
+  }
 })
