0d5c8bbd030f0c8f2410697270cd6f97e936a4ff
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-14 23:03:56 +0000

    fixed login/registration?????????

--- team_project/homepage/serializers.py
+++ team_project/homepage/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            last_name=validated_data['surname']
         )
         user.set_password(validated_data['password'])
         user.save()
