cc95310573c3e847678fab768757e575622cf005
Yugal Khanal <yugalkhanal5@gmail.com>
2023-03-14 19:00:08 +0000

    fixed login/registration?????????

--- team_project/homepage/views.py
+++ team_project/homepage/views.py
@@ -3,27 +3,32 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.views.decorators.csrf import csrf_exempt
-
+from django.http import JsonResponse
 from .serializers import UserSerializer
 
+# @csrf_exempt
+# @api_view(['POST'])
+# def register_user(request):
+#     serializer = UserSerializer(data=request.data)
+#     print("after serializer")
+#     if serializer.is_valid():
+#         print("serializer is valid")
+#         user = serializer.save()
+#         refresh = RefreshToken.for_user(user)
+#         data = {
+#             'refresh': str(refresh),
+#             'access': str(refresh.access_token),
+#         }
+#         response = Response(data, status=status.
+#         HTTP_201_CREATED)
+#         print("response is", response)
+#         response["Access-Control-Allow-Origin"] = "*"
+#         response["Access-Control-Allow-Methods"] = "POST"
+#         response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+#         return Response(data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @csrf_exempt
-@api_view(['POST'])
 def register_user(request):
-    serializer = UserSerializer(data=request.data)
-    print("after serializer")
-    if serializer.is_valid():
-        print("serializer is valid")
-        user = serializer.save()
-        refresh = RefreshToken.for_user(user)
-        data = {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }
-        response = Response(data, status=status.
-        HTTP_201_CREATED)
-        print("response is", response)
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Methods"] = "POST"
-        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        return Response(data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    print("request is", request)
+    return JsonResponse({"message": "Hello, world!"})
