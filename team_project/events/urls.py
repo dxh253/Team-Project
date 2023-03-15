5ccfe8bf311c2d235cfdbd2384bcc9d12dd000de
Praveen Rajendran <pxr247@student.bham.ac.uk>
2023-03-15 13:06:43 +0000

    tried to fix mvp presentation

--- team_project/events/urls.py
+++ team_project/events/urls.py
@@ -3,7 +3,7 @@ from .views import EventsView
 from events import views
 
 urlpatterns = [
-    path('latest-events/', views.EventsList.as_view()),
+    path('api/v1/latest-events/', views.EventsList.as_view()),
     path('events/<slug:category_slug>/<slug:events_slug>/', views.EventsDetail.as_view()),
-    path('events/', EventsView.as_view(), name = 'eventsView'),
+    path('api/v1/events/', EventsView.as_view(), name = 'eventsView'),
 ]
