692c82f7cb1e907160defdc103a8c00d060662bb
Daniel Kwok Pin Ho <dxh253@student.bham.ac.uk>
2023-03-20 17:51:15 +0000

    updated views.py:

--- team_project/forum/serializers.py
+++ team_project/forum/serializers.py
@@ -1,136 +1,54 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Subreddit, Post, PostVotes, PostComment
+from .models import Subreddit, Post, PostVotes
 
-User = get_user_model()
+from django.contrib.auth.models import User
+
+from django.contrib.auth import get_user_model
+
+class SubredditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subreddit
+        fields = (
+            "id",
+            "name",
+            "description",
+            "slug"
+        )
 
 class PostSerializer(serializers.ModelSerializer):
-    subreddit = serializers.PrimaryKeyRelatedField(
-        queryset=Subreddit.objects.all()
-    )
-
     class Meta:
         model = Post
-        fields = [
+        fields = (
             "id",
-            "created",
-            "modified",
-            'time_since_post',
             "title",
-            "link",
-            "description",
-            "description_br",
-            "owner",
-            'score',
-            "username",
-            "owner_url",
+            "content",
             "subreddit",
-            "subreddit_name",
-            'slug',
-            'subreddit_slug',
-            'full_url',
-            'user_vote',
-        ]
-        extra_kwargs = {
-            "url": {"view_name": "api:posts", "lookup_field": "title"}
-        }
-        # extra_kwargs = {
-        #     "url": {"view_name": "api:user-detail", "lookup_field": "username"}
-        # }
-
-
+            "author",
+            "slug"
+        )
+    
 class PostVotesSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = PostVotes
-        fields = ['post_id', 'user_id', 'vote', 'id',
-                  ]
-
-
-class AllPostsSerializer(serializers.ModelSerializer):
-    subreddit = serializers.PrimaryKeyRelatedField(
-        queryset=Subreddit.objects.all()
-    )
-    queryset = Post.objects.all()
-
-    class Meta:
-        model = Post
-        fields = [
+        fields = (
             "id",
-            "created",
-            "modified",
-            'time_since_post',
-            "title",
-            "link",
-            "description",
-            "description_br",
-            "owner",
-            "username",
-            "owner_url",
-            "subreddit",
-            "subreddit_name",
-            'slug',
-            'subreddit_slug',
-            'score',
-            'full_url',
-            'user_vote',
-            'user_up_style',
-            'user_down_style',
-            'weighted_score',
-            'age_in_days',
-            'number_of_comments',
-        ]
-        extra_kwargs = {
-            "url": {"view_name": "api:allposts", "lookup_field": "title"}
-        }
-        # extra_kwargs = {
-        #     "url": {"view_name": "api:user-detail", "lookup_field": "username"}
-        # }
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    children = serializers.SerializerMethodField(
-        read_only=True, method_name="get_children")
-
-    post_id = serializers.PrimaryKeyRelatedField(
-        queryset=Post.objects.all()
-    )
-    user_id = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
-    parent = serializers.PrimaryKeyRelatedField(
-        queryset=PostComment.objects.all(), allow_null=True)
-
-    class Meta:
-        model = PostComment
-        fields = [
-            "id",
-            "post_id",
-            "created",
-            "modified",
-            'time_since_comment',
-            "comment",
-            "comment_br",
-            "user_id",
-            "username",
-            "owner_url",
-            'score',
-            'user_vote',
-            'user_up_style',
-            'user_down_style',
-            'level',
-            'parent',
-            'children',
-            'tldr',
-        ]
-
-        extra_kwargs = {
-            "url": {"view_name": "api:allposts", "lookup_field": "title"}
-        }
-
-    def get_children(self, obj):
-        """ self referral field """
-        serializer = CommentSerializer(
-            instance=obj.comment_reply.all(),
-            many=True
+            "post",
+            "user",
+            "vote"
         )
-        return serializer.data
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password"
+        )
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+    
--- team_project/forum/views.py
+++ team_project/forum/views.py
@@ -1,82 +1,51 @@
-# from django.shortcuts import render
-# from django.views.generic import ListView, DetailView, TemplateView, CreateView
-# from .models import Subreddit, Post
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
+from .models import Subreddit, Post
 
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
+class PostView(DetailView):
+    model = Post
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+    template_name = 'posts/post_view.html'
 
-# from .serializers import PostSerializer, PostVotesSerializer, AllPostsSerializer
-# from rest_framework import generics
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the data
+        context['subreddits'] = Subreddit.objects.all()
+        return context
 
-# class PostView(generics.RetrieveAPIView):
-#     model = Post
-#     slug_field = 'slug'
-#     slug_url_kwarg = 'slug'
-#     template_name = 'posts/post_view.html'
+class SubredditView(DetailView):
+    model = Subreddit
+    template_name = 'posts/subreddit_view.html'
 
-#     def get_context_data(self, **kwargs):
-#         # Call the base implementation first to get a context
-#         context = super().get_context_data(**kwargs)
-#         # Add in a QuerySet of all the data
-#         context['subreddits'] = Subreddit.objects.all()
-#         return context
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the data
+        obj = self.get_object().id
+        context['posts'] = Post.objects.filter(subreddit=obj)
+        context['subreddits'] = Subreddit.objects.all()
+        return context
 
-# class SubredditView(DetailView):
-#     model = Subreddit
-#     template_name = 'posts/subreddit_view.html'
+class AllSubredditsView(TemplateView):
+    model = Subreddit
+    template_name = 'posts/subreddit_view.html'
 
-#     def get_context_data(self, **kwargs):
-#         # Call the base implementation first to get a context
-#         context = super().get_context_data(**kwargs)
-#         # Add in a QuerySet of all the data
-#         obj = self.get_object().id
-#         context['posts'] = Post.objects.filter(subreddit=obj)
-#         context['subreddits'] = Subreddit.objects.all()
-#         return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        context['subreddits'] = Subreddit.objects.all()
+        context['page'] = "all"
+        return context
 
-# class AllSubredditsView(TemplateView):
-#     model = Subreddit
-#     template_name = 'posts/subreddit_view.html'
+class FrontPageView(TemplateView):
+    model = Subreddit
+    template_name = 'posts/subreddit_view.html'
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['posts'] = Post.objects.all()
-#         context['subreddits'] = Subreddit.objects.all()
-#         context['page'] = "all"
-#         return context
-
-# class FrontPageView(TemplateView):
-#     model = Subreddit
-#     template_name = 'posts/subreddit_view.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['posts'] = Post.objects.all()
-#         context['subreddits'] = Subreddit.objects.all()
-#         context['page'] = "frontpage"
-#         return context
-
-from rest_framework import generics
-from .models import Subreddit, Post, PostVotes, PostComment
-from .serializers import PostSerializer, PostVotesSerializer, AllPostsSerializer, CommentSerializer
-
-class PostAPIView(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class PostDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    lookup_field = 'title'
-
-class AllPostsAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = AllPostsSerializer
-
-class CommentAPIView(generics.ListCreateAPIView):
-    queryset = PostComment.objects.all()
-    serializer_class = CommentSerializer
-
-class CommentDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = PostComment.objects.all()
-    serializer_class = CommentSerializer
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        context['subreddits'] = Subreddit.objects.all()
+        context['page'] = "frontpage"
+        return context
