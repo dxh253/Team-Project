00ca85003304d2c3b098a814e3a67136cdcb73c1
Xiaojin <xxj109@student.bham.ac.uk>
2023-03-29 02:54:07 +0100

    updated helpsection.vue bulma, edited App.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -32,7 +32,63 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <main>
-    <router-view />
-  </main>
+  <div id ="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>UoB Forum</strong></router-link>
+      
+      
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-end">
+          <router-link to="/events" class="navbar-item">Events</router-link>
+          <router-link to="" class="navbar-item">Discussion</router-link>
+          <router-link to="/help" class="navbar-item">Help</router-link>
+          
+      <div class="navbar-item">
+        <div class="buttons">
+          <router-link v-if="!accessToken" to="/login" class="button is-success">Log in</router-link>
+          <button v-if="accessToken" @click="logout" class="button is-success">Log out</button>
+        </div>
+      </div>
+        </div>
+      </div>
+
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p>Alpha Project Disclaimer: This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g. financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed.</p>
+      <p class="has-text-centered">Copyright (c) 2023</p>
+    </footer>
+  </div>
+  
 </template>
+
+<style lang="scss">
+  @import '../node_modules/bulma/bulma.sass'
+</style>
+
+
+  <script>
+  import { mapState } from 'vuex'
+  export default {
+    computed: mapState(['accessToken']),
+
+    methods: {
+      logout() {
+        this.$store.dispatch('userLogout')
+        this.$router.push('/')
+      },
+    },
+  }
+  </script>
--- front-end/src/views/HelpSection.vue
+++ front-end/src/views/HelpSection.vue
@@ -1,47 +1,61 @@
 <template>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
-    <!-- references to the other pages yet to be added -->
-    <!-- logo and site name yet to be added -->
-
-    <div class="htopnav">
-        <a class="material-icons" onclick="openNav()">menu</a>
-        <b>Help Section</b>
+    <div class="home">
+    <section class="hero is-small is-dark mb-6">
+          <div class="hero-body has-text-centered mt-6">
+            <h1 class="title" style="font-size: 50px;">
+               How can we help?
+            </h1>
+            <input class="input" type="text" placeholder="Search your problem here..." style="width: 675px; border-radius: 20px; text-indent: 3%;">
+          </div>
+          <div class="hero-footer has-text-centered mb-5">
+            <p>
+                Here are some useful articles
+            </p>
+          </div>
+        
+    </section>
     </div>
-
-    <!-- search needs to search title in database -->
-    <div class="hheader">
-        <a>How can we help?</a>
-        <input type="text" placeholder="Search your problem here" id="keywordProblem">
-        <c>Here are some useful articles</c>
-    </div>
-
-    <!-- buttons need appriate functions -->
-    <div class="hmain1">
-        <button type="button" class="button" onclick="button1()">Testing</button>
-        <button type="button" class="button" onclick="button2()">Testing2</button>
-        <button type="button" class="button" onclick="button3()">Testing3</button>
-    </div>
-
-    <!-- user needs to be updated according to each user -->
-    <!-- submit function needs to be properly implemented -->
-    <div class="hmain2">
-        <a>Or you can submit your problem here</a>
-        <div class="hmain21">
-            <i class="material-icons">person</i>
-            <a>User 1</a>
+    
+    <div class="columns is-mobile is-centered is-multiline has-text-centered" style="margin-bottom: 2%;">
+        <div class="column">
+            <button class="button is-large is-link is-light is-outlined" style="width: 250px; height: 125px; margin: 3px 25px;">
+                Testing
+            </button>
+            <button class="button is-large is-link is-light is-outlined" style="width: 250px; height: 125px; margin: 3px 25px;">
+                Testing
+            </button>
+            <button class="button is-large is-link is-light is-outlined" style="width: 250px; height: 125px; margin: 3px 25px;">
+                Testing232
+            </button>
         </div>
-        <form class="hmain22">
-            <input type="text" id="title" placeholder="Enter Title" class="title">
-            <textarea type="text" id="problem" placeholder="Describe your problem here" class="problem"></textarea>
-            <button type="button" class="button" onclick="submit()">Submit</button>
-        </form>
     </div>
+    
+    <div class="hero" style="margin-bottom: 3%; margin-left: 5%;">
+        <p class="title is-4">Or you can submit your problem here</p>
+        <div class="columns" style="align-items: center;">
+            <i class="column is-1 material-icons" style="font-size: 40px;">person</i>
+        </div>
+
+        <div class="event-card" style="margin: 0%; width: 1000px;">
+            <div class="field">
+                <label>Title of your problem</label>
+                <input class="input" rows="1" type="text" id="title" placeholder="Enter Title" style="width: 400px; border:1px solid grey; background-color: white;">
+            </div>
+            <div class="field">
+                <label>Describe your problem</label>
+                <textarea class="textarea" type="text" id="problem" placeholder="Describe your problem here" style="border:1px solid grey;"></textarea>
+                <div style="text-align: right;">
+                <button class="button is-info" type="button" onclick="submit()" style="margin-top: 5px;">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h2 class="title is-3">Here is a list of problems submitted by other users</h2>
+
 </template>
 
-<style>
-@import url('../assets/HelpSectionView.css');
-</style>
 
 <script>
 export default {
