a13c6e7d9cccd631431554af23ba9265247c695d
yugalkhanal <yugalkhanal5@gmail.com>
2023-04-07 00:55:17 +0100

    added saved events section in dashboard

--- front-end/src/views/DashBoard.vue
+++ front-end/src/views/DashBoard.vue
@@ -1,30 +1,71 @@
 <template>
-    <div class="main2">
-        <div class="event-card">
-            <router-link to="/events">Events</router-link>
 
+    <div>
+      <div class="main2">
+        <div class="event-card">
+          <router-link to="/events">Events</router-link>
         </div>
         <div class="event-card">
-            <router-link to="/discussions">Discussions</router-link>
+          <router-link to="/discussions">Discussions</router-link>
         </div>
         <div class="event-card">
-            <router-link to="/resources">Resources</router-link>
+          <router-link to="/resources">Resources</router-link>
         </div>
         <div class="event-card">
-            <router-link to="/study-group">Study group</router-link>
+          <router-link to="/study-group">Study group</router-link>
         </div>
         <div class="event-card">
-            <router-link to="/help">Help</router-link>
+          <router-link to="/help">Help</router-link>
         </div>
+      </div>
+      <div class="your-events">
+        <h3>Your saved events:</h3>
+        <ul>
+            <li v-for="event in userEvents" :key="event.id">
+            <router-link :to="'' + event.event.get_absolute_url">
+            {{ event.event.name }}
+            </router-link>
+            </li>
+        </ul>
+
+
+      </div>
     </div>
-</template>
-
-<script>
+  </template>
+  
+  <script>
+ import { getAPI } from '@/plugins/axios'
+  
   export default {
-      name: 'DashBoard',
-      data(){
-
+    name: 'DashBoard',
+    data() {
+      return {
+        userEvents: []
+      };
+    },
+    created() {
+      this.getUserEvents();
+    },
+    methods: {
+      getUserEvents() {
+        getAPI.get('/api/v1/saved-events/', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access')}`
+          }
+        })
+          .then(response => {
+            this.userEvents = response.data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
       }
+    }
+  };
+  </script>
+  
+  <style>
+  .your-events {
+    margin-top: 20px;
   }
-
-</script>
+  </style>
