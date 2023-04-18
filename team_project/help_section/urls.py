7e7f57cb52a56fd3f79e7aee4286f44f743f487b
Xiaojin <xxj109@student.bham.ac.uk>
2023-04-18 15:23:52 +0100

    modified urls

--- team_project/help_section/urls.py
+++ team_project/help_section/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+
 urlpatterns = [
-    path('api/v1/problems', views.ProblemsView.as_view()),
+    path('api-token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api-token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/help/', views.ProblemsView.as_view(), name="ProblemView"),
 ]
