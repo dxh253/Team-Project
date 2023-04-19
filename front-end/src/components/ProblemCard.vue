943aa515f70df1f16acc7b48b5720aa88272f09e
Xiaojin <xxj109@student.bham.ac.uk>
2023-04-20 00:09:24 +0100

    prevent description overflow

--- front-end/src/components/ProblemCard.vue
+++ front-end/src/components/ProblemCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="box" @click.right="viewProblem" style="max-width: 1000px; margin-left: 5%;">    
         <h3 class="is-size-4"> {{ problem.title }} </h3>
-        <p class="is-size-6"> {{ problem.description }}</p>
+        <p class="is-size-6" style="overflow: hidden; text-overflow: ellipsis; margin-right:10% ;"> {{ problem.description }}</p>
     </div>
 </template>
 
