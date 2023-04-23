0b021ba60e93f29c0d79b83483617e8d9f5b06b3
Yugal Khanal <yugalkhanal5@gmail.com>
2023-04-23 20:49:07 +0100

    added all features until now

--- front-end/src/components/AccessibilityPage.vue
+++ front-end/src/components/AccessibilityPage.vue
@@ -46,17 +46,20 @@ export default {
 .accessibility {
     display: flex;
     gap: 10px;
+    padding: 10px;
+
 }
 
 .accessibility__button {
     background-color: #fff;
     border: none;
-    border-radius: 5px;
+    border-radius: 15px;
     color: #333;
     cursor: pointer;
     font-size: 1.5rem;
-    height: 50px;
-    width: 50px;
+    height: 40px;
+    width: 40px;
+    align-items: center;
 }
 
 .accessibility__button:hover,
@@ -67,14 +70,92 @@ export default {
 
 .large-text {
     font-size: 1.2rem;
+    font-weight: bold;
+}
+
+.large-text button {
+    font-size: 1.2rem;
+    font-weight: bold;
+
 }
 
 .high-contrast {
     background-color: #000;
-    color: #fff;
+    color: turquoise;
+}
+
+.high-contrast a {
+    color: turquoise;
+    text-decoration: underline;
+}
+
+.high-contrast div {
+    color: white;
+    background-color: black;
+}
+
+.high-contrast label,
+.high-contrast p,
+.high-contrast span {
+    color: white !important;
+}
+
+.high-contrast form {
+    background-color: black;
+    color: white;
+    border: solid 1px;
+}
+
+.high-contrast input {
+    background-color: black;
+    color: white;
+    border: solid 1px;
+}
+
+.high-contrast button {
+    background-color: black !important;
+    color: white !important;
+    border: solid 1px !important;
+}
+
+.high-contrast div.column {
+    background-color: black;
+    border: solid 1px;
+}
+
+.high-contrast h1,
+h2,
+title {
+    color: white;
+}
+
+.high-contrast h2.title {
+    /*specifically for help section*/
+    color: white;
+}
+
+.high-contrast h2.subtitle {
+    /*specifically for comment section*/
+    color: white;
+}
+
+.high-contrast footer {
+    background-color: black;
+    color: white;
+}
+
+.high-contrast div.event-card {
+    border: block 1px;
+}
+
+.high-contrast input::placeholder {
+    color: black;
+}
+
+.high-contrast textarea::placeholder {
+    color: black;
 }
 
 .no-focus {
     outline: none;
-}
-</style>
+}</style>
