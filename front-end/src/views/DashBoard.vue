6aeb2b3ad403d4914de238a91e37000471ff6f75
Praveen Rajendran <pxr247@student.bham.ac.uk>
2023-04-24 23:37:52 +0100

    changed css for dashboard, now pretty like kendall jenner

--- front-end/src/views/DashBoard.vue
+++ front-end/src/views/DashBoard.vue
@@ -443,7 +443,7 @@ export default {
 </style> -->
 
 
-<template>
+<!-- <template>
   <div class="container">
     <div class="columns">
       <div class="column is-4">
@@ -587,4 +587,388 @@ export default {
 .event-reminder {
   margin-left: auto;
 }
+</style> -->
+
+
+<!-- <template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-4">
+        <div class="card event-card has-background-primary">
+          <router-link v-a11y-link="'Events'" to="/events"
+            class="card-content has-text-white is-size-3 has-text-weight-bold has-text-centered">
+            <i class="far fa-calendar is-block  fa-5x has-text-white"></i> Events and Study
+            group
+          </router-link>
+        </div>
+      </div>
+      <div class="column is-4">
+        <div class="card event-card has-background-primary">
+          <router-link v-a11y-link="'Discussions'" to="/posts"
+            class="card-content has-text-white is-size-3 has-text-weight-bold has-text-centered">
+            <i class="fas fa-file-alt is-block fa-5x has-text-white"> </i> Resources and
+            Discussions
+          </router-link>
+        </div>
+      </div>
+      <div class="column is-4">
+        <div class="card event-card has-background-primary">
+          <router-link v-a11y-link="'Help'" to="/help"
+            class="card-content has-text-white is-size-3 has-text-weight-bold has-text-centered">
+            <i class="fa fa-question-circle is-block fa-5x has-text-white"></i> Help
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-half">
+        <div class="box">
+          <h3 class="title is-3 has-text-weight-bold">
+            <i class="fas fa-bookmark has-text-primary"></i> SAVED EVENTS:
+          </h3>
+          <ul class="has-text-weight-bold">
+            <li v-for="event in userEvents" :key="event.id">
+              <router-link :to="'' + event.event.get_absolute_url" v-a11y-link="event.event.name" class="event-link"
+                v-if="event.event.category === 1">
+                <i class="far fa-calendar event-icon has-text-primary"></i> {{ event.event.name }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="column is-half">
+        <div class="box">
+          <h3 class="title is-3 has-text-weight-bold">
+            <i class="fas fa-bookmark has-text-primary"></i> SAVED STUDY GROUP:
+          </h3>
+          <ul class="has-text-weight-bold">
+            <li v-for="event in userEvents" :key="event.id">
+              <router-link :to="event.event.get_absolute_url" v-a11y-link="event.event.name" class="event-link" v-if="event.event.category === 2">
+              <i class="fas fa-users event-icon has-text-primary"></i> {{ event.event.name }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+    </div>
+      </div>
+      <div class="box">
+        <h3 class="title is-3 has-text-weight-bold">
+          <i class="fas fa-arrow-up-from-bracket has-text-primary"></i> UPCOMING:
+        </h3>
+        <ul class="has-text-weight-bold">
+          <li v-for="event in userEvents" :key="event.id">
+            <router-link :to="'' + event.event.get_absolute_url" v-a11y-link="event.event.name" class="event-link">
+              <i class="fas fa-clock event-icon has-text-primary"></i>
+              <span class="event-name">{{ event.event.name }}</span>
+              <span class="event-reminder">{{ getReminder(event.event.date) }}</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+</template>
+<script>
+import { getAPI } from '@/plugins/axios'
+
+export default {
+  name: 'DashBoard',
+  data() {
+    return {
+      userEvents: []
+    };
+  },
+  created() {
+    this.getUserEvents();
+  },
+  methods: {
+    getUserEvents() {
+      getAPI.get('/api/v1/saved-events/', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access')}`
+        }
+      })
+        .then(response => {
+          this.userEvents = response.data.sort((b, a) => {
+            return new Date(a.event.date) - new Date(b.event.date);
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getReminder(eventDate) {
+      const m = 24 * 60 * 60 * 1000;
+      const a = new Date();
+      const t = new Date(eventDate).getTime();
+      const r = t - m;
+      const d = r - a.getTime();
+      const dd = Math.abs(Math.floor(d / m));
+      return `${dd} day${dd > 1 ? 's' : ''} left`;
+    }
+  }
+};
+</script>
+<style scoped>
+.event-card {
+  border-radius: 0.25rem;
+  margin-bottom: 1.5rem;
+}
+
+.event-icon {
+  margin-right: 0.5rem;
+}
+
+.event-name {
+  font-weight: bold;
+}
+.event-link {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+}
+.event-reminder {
+  margin-left: auto;
+}
+
+.columns {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.box {
+  height: 300px;
+  overflow-y: auto;
+}
+</style> -->
+
+<template>
+   <div class="container">
+    <section class="hero is-medium is-dark mb-6 is-fullwidth" style="padding: 0 20px;">
+      <div class="columns is-multiline is-mobile">
+        <div class="column is-full-mobile">
+          <button class="button is-primary event-card is-small is-fullwidth" style="height: 150px;">
+            <router-link v-a11y-link="'Events'" to="/events"
+              class="card-content has-text-white is-size-5 has-text-weight-bold has-text-centered is-flex is-flex-direction-column is-align-items-center is-justify-content-center"
+              style="width: 100%;">
+              <div class="is-flex is-align-items-center is-justify-content-center">
+                <i class="far fa-calendar is-block fa-3x has-text-white is-hidden-mobile"></i>
+              </div>
+              <div style="margin-top: 0.5rem;">
+                Events and Study
+              </div>
+            </router-link>
+          </button>
+        </div>
+        <div class="column is-full-mobile">
+          <button class="button is-primary event-card is-small is-fullwidth" style="height: 150px;">
+            <router-link v-a11y-link="'Discussions'" to="/posts"
+              class="card-content has-text-white is-size-5 has-text-weight-bold has-text-centered is-flex is-flex-direction-column is-align-items-center is-justify-content-center"
+              style="width: 100%;">
+              <div class="is-flex is-align-items-center is-justify-content-center">
+                <i class="fas fa-file-alt is-block fa-3x has-text-white is-hidden-mobile"></i>
+              </div>
+              <div style="margin-top: 0.5rem;">
+                Discussions
+              </div>
+            </router-link>
+          </button>
+        </div>
+        <div class="column is-full-mobile">
+          <button class="button is-primary event-card is-small is-fullwidth" style="height: 150px;">
+            <router-link v-a11y-link="'Help'" to="/help"
+              class="card-content has-text-white is-size-5 has-text-weight-bold has-text-centered is-flex is-flex-direction-column is-align-items-center is-justify-content-center"
+              style="width: 100%;">
+              <div class="is-flex is-align-items-center is-justify-content-center">
+                <i class="fa fa-question-circle is-block fa-3x has-text-white is-hidden-mobile"></i>
+              </div>
+              <div style="margin-top: 0.5rem;">
+                Help
+              </div>
+            </router-link>
+          </button>
+        </div>
+      </div>
+    </section>
+
+
+
+
+    <div class="columns">
+      <div class="column is-one-third">
+        <div class="box">
+          <h3 class="title is-3 has-text-weight-bold">
+            <i class="fas fa-bookmark has-text-primary"></i> SAVED EVENTS:
+          </h3>
+          <ul class="has-text-weight-bold">
+            <li v-for="event in userEvents" :key="event.id">
+              <router-link :to="'' + event.event.get_absolute_url" v-a11y-link="event.event.name" class="event-link"
+                v-if="event.event.category === 1">
+                <i class="far fa-calendar event-icon has-text-primary"></i> {{ event.event.name }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="column is-one-third">
+        <div class="box">
+          <h3 class="title is-3 has-text-weight-bold">
+            <i class="fas fa-bookmark has-text-primary"></i> SAVED STUDY GROUP:
+          </h3>
+          <ul class="has-text-weight-bold">
+            <li v-for="event in userEvents" :key="event.id">
+              <router-link :to="event.event.get_absolute_url" v-a11y-link="event.event.name" class="event-link" v-if="event.event.category === 2">
+              <i class="fas fa-users event-icon has-text-primary"></i> {{ event.event.name }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="column is-one-third">
+        <div class="box">
+          <h3 class="title is-3 has-text-weight-bold">
+            <i class="fas fa-arrow-up-from-bracket has-text-primary"></i> UPCOMING:
+          </h3>
+          <ul class="has-text-weight-bold">
+            <li v-for="event in userEvents" :key="event.id">
+              <router-link :to="'' + event.event.get_absolute_url" v-a11y-link="event.event.name"
+              class="event-link">
+                <i class="fas fa-clock event-icon has-text-primary"></i>
+                <span class="event-name">{{ event.event.name }}</span>
+                <span class="event-reminder">{{ getReminder(event.event.date) }}</span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+<script>
+import { getAPI } from '@/plugins/axios'
+
+export default {
+  name: 'DashBoard',
+  data() {
+    return {
+      userEvents: []
+    };
+  },
+  created() {
+    this.getUserEvents();
+  },
+  methods: {
+    getUserEvents() {
+      getAPI.get('/api/v1/saved-events/', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access')}`
+        }
+      })
+        .then(response => {
+          this.userEvents = response.data.sort((b, a) => {
+            return new Date(a.event.date) - new Date(b.event.date);
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getReminder(eventDate) {
+      const m = 24 * 60 * 60 * 1000;
+      const a = new Date();
+      const t = new Date(eventDate).getTime();
+      const r = t - m;
+      const d = r - a.getTime();
+      const dd = Math.abs(Math.floor(d / m));
+      return `${dd} day${dd > 1 ? 's' : ''} left`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.event-card {
+  border-radius: 0.25rem;
+  margin-bottom: 3.5rem;
+}
+
+.event-card .card-content {
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+}
+.event-link {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  color: #3273dc;
+}
+
+.event-link:hover {
+  text-decoration: underline;
+}
+
+.event-icon {
+  margin-right: 0.5rem;
+}
+
+.event-name {
+  font-weight: bold;
+  margin-left: 0.5rem;
+}
+
+.event-reminder {
+  margin-left: auto;
+}
+
+.box {
+  height: 300px;
+  overflow-y: auto;
+  margin-bottom: 2rem;
+}
+
+.box .title {
+  font-weight: bold;
+  color: #3273dc;
+}
+
+.box ul {
+  margin-top: 1.5rem;
+}
+
+.box ul li {
+  margin-bottom: 0.5rem;
+}
+
+.box ul li:last-child {
+  margin-bottom: 0;
+}
+
+
+@media screen and (max-width: 768px) {
+  .event-card {
+    height: 100px;
+    font-size: 1rem;
+    padding: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+
+  .event-card .card-content {
+    font-size: 1rem;
+  }
+
+  .event-link {
+    margin-bottom: 0;
+  }
+
+  .event-icon {
+    display: none;
+  }
+
+  .box {
+    height: auto;
+  }
+}
 </style>
+
