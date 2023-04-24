0a6659ca24acb6bb90043ae0ee7e47d4c7cfcf6a
siddhantjogai <sxj019@student.bham.ac.uk>
2023-04-24 16:33:30 +0100

    Format with prettier

--- front-end/src/components/CommentBox.vue
+++ front-end/src/components/CommentBox.vue
@@ -1,154 +1,176 @@
 <template>
-    <div class="comment-container">
-        <div class="comments">
-            <div v-if="comments.length === 0">No comments yet.</div>
-            <div v-else>
-                <div v-for="comment in comments" :key="comment.id" class="comment">
-                    <div class="comment-header">
-                        <h3 class="comment-owner">{{ comment.owner }}</h3>
-                    </div>
-                    <div class="comment-body">
-                        <p>{{ comment.text }}</p>
-                    </div>
-                    <button @click="showReplyForm(comment.id)" class="button is-small is-light">Reply</button>
-                    <div v-if="visibleReplyForm === comment.id" class="reply-form">
-                        <textarea class="textarea" v-model="replyText[comment.id]"
-                            placeholder="Write your reply here"></textarea>
-                        <button @click="submitReply(comment.id)" class="button is-small is-primary">Submit</button>
-                    </div>
-                    <div class="replies" v-if="comment.children && comment.children.length > 0">
-                        <div v-for="reply in comment.children" :key="reply.id" class="reply">
-                            <div class="reply-footer">
-                                <span class="reply-owner">{{ reply.owner }}</span>
-                            </div>
-                            <div class="reply-body">
-                                <p>{{ reply.text }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <div class="comment-container">
+    <div class="comments">
+      <div v-if="comments.length === 0">No comments yet.</div>
+      <div v-else>
+        <div v-for="comment in comments" :key="comment.id" class="comment">
+          <div class="comment-header">
+            <h3 class="comment-owner">{{ comment.owner }}</h3>
+          </div>
+          <div class="comment-body">
+            <p>{{ comment.text }}</p>
+          </div>
+          <button
+            @click="showReplyForm(comment.id)"
+            class="button is-small is-light"
+          >
+            Reply
+          </button>
+          <div v-if="visibleReplyForm === comment.id" class="reply-form">
+            <textarea
+              class="textarea"
+              v-model="replyText[comment.id]"
+              placeholder="Write your reply here"
+            ></textarea>
+            <button
+              @click="submitReply(comment.id)"
+              class="button is-small is-primary"
+            >
+              Submit
+            </button>
+          </div>
+          <div
+            class="replies"
+            v-if="comment.children && comment.children.length > 0"
+          >
+            <div
+              v-for="reply in comment.children"
+              :key="reply.id"
+              class="reply"
+            >
+              <div class="reply-footer">
+                <span class="reply-owner">{{ reply.owner }}</span>
+              </div>
+              <div class="reply-body">
+                <p>{{ reply.text }}</p>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "CommentBox",
-    props: {
-        comments: {
-            type: Array,
-            default: () => [],
-        },
+  name: "CommentBox",
+  props: {
+    comments: {
+      type: Array,
+      default: () => [],
     },
-    data() {
-        return {
-            visibleReplyForm: null,
-            replyText: {},
-        };
+  },
+  data() {
+    return {
+      visibleReplyForm: null,
+      replyText: {},
+    };
+  },
+  methods: {
+    showReplyForm(commentId) {
+      this.visibleReplyForm =
+        this.visibleReplyForm === commentId ? null : commentId;
     },
-    methods: {
-        showReplyForm(commentId) {
-            this.visibleReplyForm = this.visibleReplyForm === commentId ? null : commentId;
-        },
-        submitReply(commentId) {
-            this.$emit('add-reply', {
-                parentCommentId: commentId,
-                text: this.replyText[commentId],
-            });
-            this.replyText[commentId] = '';
-            this.visibleReplyForm = null;
-        },
+    submitReply(commentId) {
+      this.$emit("add-reply", {
+        parentCommentId: commentId,
+        text: this.replyText[commentId],
+      });
+      this.replyText[commentId] = "";
+      this.visibleReplyForm = null;
     },
+  },
 };
 </script>
 
 <style lang="scss">
 .comment-container {
-    margin-top: 1.5rem;
+  margin-top: 1.5rem;
 }
 
 .comment,
 .reply {
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    padding: 1rem;
-    margin-bottom: 1rem;
-    display: flex;
-    flex-direction: column;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
 }
 
 .comment-header,
 .reply-header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
 }
 
 .comment-owner {
-    font-size: 1.125rem;
-    font-weight: bold;
-    margin-right: 0.5rem;
+  font-size: 1.125rem;
+  font-weight: bold;
+  margin-right: 0.5rem;
 }
 
 .comment-body,
 .reply-body {
-    font-size: 1rem;
-    line-height: 1.5;
-    color: #555;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #555;
 }
 
 .reply {
-    border-color: #eee;
-    margin-left: 2rem;
-    margin-top: 1rem;
+  border-color: #eee;
+  margin-left: 2rem;
+  margin-top: 1rem;
 }
 
 .reply-body {
-    margin-bottom: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 
 .reply-footer {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 0.5rem;
 }
 
 .reply-owner {
-    font-size: 0.875rem;
-    font-weight: bold;
-    color: #555;
+  font-size: 0.875rem;
+  font-weight: bold;
+  color: #555;
 }
 
 .reply-form {
-    margin-top: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
+  margin-top: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 }
 
 .reply-form .textarea {
-    min-height: 3rem;
+  min-height: 3rem;
 }
 
 .reply-form button {
-    margin-top: 0.5rem;
+  margin-top: 0.5rem;
 }
 
 .button.is-light {
-    border-color: #ccc;
-    color: #333;
+  border-color: #ccc;
+  color: #333;
 }
 
 .button.is-light:hover {
-    background-color: #f5f5f5;
+  background-color: #f5f5f5;
 }
 
 .button.is-primary {
-    background-color: #4a4a4a;
+  background-color: #4a4a4a;
 }
 
 .button.is-primary:hover {
-    background-color: #363636;
-}</style>
+  background-color: #363636;
+}
+</style>
