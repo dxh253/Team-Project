6f4491cfe5d5a3d66c8e0c8b3f5030daa84fdd18
Yugal Khanal <yck104@student.bham.ac.uk>
2023-04-26 18:59:43 +0000

    All features small fixes

---
+++ front-end/src/components/ProblemComment.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="box" style="margin: 1% 10%; max-width: 70%;">
+        <p style="color:gray;">{{ comment.date }} by {{ comment.username }}</p>
+        <p style="margin:0% 3%; overflow-wrap: break-word;">{{ comment.text }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProblemComment",
+        props: ["comment"],
+    }
+</script>
--- front-end/src/views/ProblemsDetail.vue
+++ front-end/src/views/ProblemsDetail.vue
@@ -9,14 +9,25 @@
                     style="font-size: 35px; display: grid; align-self: center; text-align: end;"
                     @click="deleteProblem">delete</i>
             </div>
-            <p class="is-size-6" style="text-indent: 5%;">{{ problems.date_added }}</p>
-            <p class="is-size-6" style="text-indent: 5%;">{{ problems.description }}</p>
+            <p class="is-size-6" style="margin-left: 4%; margin-bottom: 1%;">{{ problems.date_added }} by {{ problems.author
+            }}</p>
+            <p class="is-size-6" style="margin-left: 4%; margin-right: 20%; overflow-wrap: break-word;">{{
+                problems.description }}</p>
         </div>
-
+        <form class="box" @submit.prevent="submitComment" style="margin:1% 10%; min-width: 70%; max-width: 70%;">
+            <textarea class="textarea" v-model="commentInfo.text" placeholder="add response"></textarea>
+            <div style="text-align: right; margin-top: 5px;">
+                <button class="button is-info" type="submit"> Submit </button>
+            </div>
+        </form>
+        <div>
+            <ProblemComment v-for="comment in allComments" :comment="comment" :key="comment.id" />
+        </div>
     </div>
 </template>
 
 <script>
+import ProblemComment from '@/components/ProblemComment.vue';
 import { getAPI } from '@/plugins/axios';
 import jwt_decode from "jwt-decode";
 
@@ -26,10 +37,18 @@ export default {
         return {
             problems: {},
             owned: false,
+            allComments: [],
+            commentInfo: {
+                text: '',
+            }
         }
     },
+    components: {
+        ProblemComment,
+    },
     mounted() {
-        this.getProblems()
+        this.getProblems();
+        this.getComments();
     },
     methods: {
         getProblems() {
@@ -46,10 +65,12 @@ export default {
                     if (this.problems.owner === userId) {
                         this.owned = true;
                     }
+                    console.log(this.problems);
                 })
                 .catch(error => {
                     console.log(error)
                 })
+
         },
 
         deleteProblem() {
@@ -69,8 +90,61 @@ export default {
                         console.log(error);
                     }
                 })
+        },
+
+        getComments() {
+            console.log("trying to get comments")
+            const problem_id = this.$route.params.problem_id
+
+            getAPI
+                .get(`/api/v1/help/${problem_id}/comments/`, {
+                    headers: { Authorization: `Bearer ${this.$store.state.accessToken}` },
+                })
+                .then((response) => {
+                    console.log('API respinse data:', response.data);
+                    this.allComments = response.data;
+                    this.$store.state.APIData = response.data;
+                })
+                .catch((error) => {
+                    console.log('API error:', error);
+                });
+        },
+
+        submitComment() {
+            console.log("Submit comment");
+            const problem_id = this.$route.params.problem_id
+            const token = localStorage.getItem("access");
+            const decodedToken = jwt_decode(token);
+            const ownerId = decodedToken.user_id;
+
+            if (!this.$store.getters.loggedIn) {
+                //User is not logged in
+                return;
+            }
+
+            console.log(this.commentInfo.text)
+
+            const formData = new FormData();
+            formData.append('problem', problem_id)
+            formData.append('text', this.commentInfo.text)
+            formData.append('author', ownerId)
+            getAPI
+                .post(`api/v1/help/${problem_id}/comments/`, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Authorization': `Bearer ${this.$store.state.accessToken}`,
+                    }
+                })
+                .then((response) => {
+                    console.log('API response data:', response.data);
+                    window.location.reload()
+                })
+                .catch((error) => {
+                    console.log(error.response.data);
+                    alert('Something went wrong. Please try again.')
+                })
         }
-    }
+    },
 }
 </script>
 
