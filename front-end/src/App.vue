1f585674ba6a0b3ef6751aa3aa901b05dc288461
yugalkhanal <yugalkhanal5@gmail.com>
2023-04-26 19:38:12 +0100

    combined features

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,73 +3,72 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <div id ="wrapper">
+  <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <router-link to="/dashboard" class="navbar-item"><strong>UoB Forum</strong></router-link>
-      
-      
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+        <router-link to="/dashboard" class="navbar-item"><strong>BrumConnect</strong></router-link>
+
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
         <div class="navbar-end">
           <router-link to="/events" class="navbar-item">Events</router-link>
           <router-link to="/posts" class="navbar-item">Discussion</router-link>
           <router-link to="/dashboard" class="navbar-item">Dashboard</router-link>
-          <Accessibility/>
+          <Accessibility />
 
-      <div class="navbar-item">
-        <div class="buttons">
-          <router-link v-if="!accessToken" to="/login" class="button is-success">Log in</router-link>
-          <button v-if="accessToken" @click="logout" class="button is-success">Log out</button>
-        </div>
-      </div>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link v-if="!accessToken" to="/login" class="button is-success">Log in</router-link>
+              <button v-if="accessToken" @click="logout" class="button is-success">Log out</button>
+            </div>
+          </div>
         </div>
       </div>
 
     </nav>
 
     <section class="section">
-      <router-view/>
+      <router-view />
     </section>
 
     <footer class="footer">
-      <p>Alpha Project Disclaimer: This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g. financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed.</p>
+      <p>Alpha Project Disclaimer: This server is provided by the School of Computer Science at the University of
+        Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we
+        take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter
+        any real personal data (e.g. financial information or otherwise) into the system. The assignment runs until May
+        31st 2023, at which time the server and all associated data will be destroyed.</p>
       <p class="has-text-centered">Copyright (c) 2023</p>
     </footer>
   </div>
-  
 </template>
 
 <style lang="scss">
-  @import '../node_modules/bulma/bulma.sass';
+@import '../node_modules/bulma/bulma.sass';
 </style>
 
 
-  <script>
-  import { mapState } from 'vuex';
-  import Accessibility from '@/components/AccessibilityPage.vue';
+<script>
+import { mapState } from 'vuex';
+import Accessibility from '@/components/AccessibilityPage.vue';
 
-  export default {
-    data() {
-      return {
-        showMobileMenu: false,
-      };
+export default {
+  computed: mapState(['accessToken']),
+  components: {
+    Accessibility
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('userLogout');
+      this.$router.push('/');
     },
-    computed: mapState(['accessToken']),
-    components:{
-      Accessibility
-    },
-    methods: {
-      logout() {
-        this.$store.dispatch('userLogout');
-        this.$router.push('/');
-      },
-    },
-  }
-  </script>
+  },
+}
+</script>
--- front-end/src/views/PostsList.vue
+++ front-end/src/views/PostsList.vue
@@ -1,11 +1,165 @@
+<!-- <template>
+  <div class="container">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <h1 class="title">Looking For Answers?</h1>
+        <p class="subtitle">Ask away!</p>
+      </div>
+    </section>
+    <div class="columns is-custom-width">
+      <div class="column">
+        <h2 class="title is-4" style="margin-bottom: 1rem; display: flex; align-items: center;">
+          <span style="flex-grow: 1;">Posts</span>
+          <router-link to="/create">
+            <button class="button is-primary">New Post</button>
+          </router-link>
+        </h2>
+        <div class="searchbar">
+          <input class="search" type="text" v-model="searchTerm" placeholder="Search posts">
+        </div>
+        <div v-if="filteredPosts.length > 0">
+          <div v-for="post in filteredPosts" :key="post.id">
+            <div class="box">
+              <div class="columns is-vcentered">
+                <div class="column">
+                  <post-box :post="post" @postDeleted="removePostFromList" @vote-updated="updatePostVote" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <button @click="scrollToTop" class="button is-info is-hidden-desktop is-fullwidth">Scroll to top</button>
+        </div>
+      </div>
+    </div>
+    <button @click="scrollToTop" class="myBtn is-hidden-mobile">Scroll to top</button>
+  </div>
+</template>
+
+
+
+
+
+
+
+<script>
+import { getAPI } from '@/plugins/axios';
+import PostBox from '../components/PostBox.vue';
+
+export default {
+    name: 'PostsList',
+    components: {
+        'post-box': PostBox,
+    },
+    data() {
+        return {
+            allposts: [],
+            searchTerm: '',
+
+
+        }
+    },
+    computed: {
+        filteredPosts() {
+            // if there is no search term, return all posts
+            if (!this.searchTerm) {
+                return this.allposts;
+            }
+            // if there are no posts, return an empty array
+            if (this.allposts.length === 0) {
+                return [];
+            }
+            // if there is a search term, filter posts that include the search term in the title
+            return this.allposts.filter(post => {
+                // filter posts that include the search term in the title
+                return post.title.toLowerCase().includes(this.searchTerm.toLowerCase());
+            });
+        }
+    },
+    methods: {
+        scrollToTop() {
+            // Scroll to top with smooth behavior
+            window.scrollTo({ top: 0, behavior: 'instant' });
+        },
+        removePostFromList(postId) {
+            this.allposts = this.allposts.filter((post) => post.id !== postId);
+        },
+        updatePostVote(postId, vote) {
+            const postToUpdate = this.allposts.find((post) => post.id === postId);
+            if (postToUpdate) {
+                postToUpdate.score += vote;
+            }
+        },
+        updatePostScore(postId, updatedScore) {
+            const postToUpdate = this.allposts.find((post) => post.id === postId);
+            if (postToUpdate) {
+                postToUpdate.score = updatedScore;
+            }
+        }
+    },
+    created() {
+        const token = localStorage.getItem("access");
+
+        getAPI
+            .get("/api/v1/posts/", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                console.log("Post API has received data");
+                this.allposts = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+}
+</script>
+
+<style lang="scss">
+.box {
+    margin-bottom: 1.5rem;
+}
+
+@media screen and (max-width: 1023px) {
+    .column.is-one-quarter-desktop {
+        display: none;
+    }
+
+    .button.is-hidden-desktop {
+        display: block !important;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+}
+
+.myBtn {
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    border: none;
+    outline: none;
+    background-color: hsl(171, 100%, 41%);
+    color: white;
+    cursor: pointer;
+    padding: 15px;
+    border-radius: 5px;
+    font-size: 18px;
+}
+</style> -->
+
 <template>
     <div class="container">
+        <section class="hero is-medium is-dark mb-6">
+            <div class="hero-body has-text-centered">
+                <h1 class="title">Looking For Answers?</h1>
+                <p class="subtitle">Ask away!</p>
+            </div>
+        </section>
         <div class="columns is-custom-width">
-            <!-- <router-link to="/dashboard">
-                <button>
-                    <i class="fas fa-arrow-left"></i>
-                </button>
-            </router-link> -->
             <div class="column">
                 <h2 class="title is-4" style="margin-bottom: 1rem; display: flex; align-items: center;">
                     <span style="flex-grow: 1;">Posts</span>
@@ -72,7 +226,6 @@ export default {
             });
         },
     },
-
     methods: {
         scrollToTop() {
             // Scroll to top with smooth behavior
