1cda984ed45c6d5f33113a0fc30a12f82bdb7cfd
yugalkhanal <yugalkhanal5@gmail.com>
2023-04-28 20:26:32 +0100

    added email unique verification

--- team_project/homepage/serializers.py
+++ team_project/homepage/serializers.py
@@ -2,13 +2,35 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
+# class UserSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(write_only=True)
+
+#     def create(self, validated_data):
+#         user = User.objects.create(
+#             username=validated_data['username'],
+#             email=validated_data['email'],
+#             first_name=validated_data['first_name'],
+#             last_name=validated_data['last_name']
+#         )
+#         user.set_password(validated_data['password'])
+#         user.save()
+#         return user
+
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
+        email = validated_data['email']
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("This email is already in use.")
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=email,
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name']
         )
@@ -18,4 +40,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'username', 'email',
+                  'first_name', 'last_name', 'password']
