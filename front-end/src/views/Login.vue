48af15f73418947c6e677c991888e6bd6d9ba8f4
Praveen Rajendran <pxr247@student.bham.ac.uk>
2023-04-30 05:17:55 +0100

    modified the frontend for login and registration

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <div class="main3">
         <p v-if="sessionExpired" class="session-expired">Your session has expired, please log in again.</p>
@@ -6,7 +6,7 @@
         <form @submit.prevent="login" class="login-form">
             <div class="form-group">
                 <label for="email">Username:</label>
-                <input type="text" id="email" name="email" v-model="username" required>
+                <input type="text" id="email" name="username" v-model="username" required>
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
@@ -24,30 +24,7 @@
         </form>
     </div>
 
-    <div class="main2">
-        <div class="event-card">
-            <h3>Study Groups</h3>
-            <p class="description">Join different study groups based on your course or academic interests and collaborate
-                with peers to learn
-            </p>
-        </div>
-        <div class="event-card">
-            <h3>Resource</h3>
-            <p class="description">Access and share a wealth of academic resources, such as study notes, textbooks, videos,
-                and other materials.
-            </p>
-        </div>
-        <div class="event-card">
-            <h3>Discussion board</h3>
-            <p class="description">Engage in discussions and debates with fellow students on a wide range of academic and
-                campus life topics.
-            </p>
-        </div>
-    </div>
 </template>
-
-<style>@import url('./../assets/HomeView.css');</style>
-
 
 <script>
 // import axios from 'axios';
@@ -84,4 +61,123 @@ export default {
         }
     }
 }   
-</script>
+</script> -->
+
+
+  
+<template>
+    <div class="container">
+        <div class="box">
+        <div class="columns">
+          <div class="column is-half">
+            <div class="description">
+              <h1 class="title">Welcome to BrumConnect</h1>
+            </div>
+            <hr>
+            <div class="columns">
+              <div class="column is-half-desktop">
+                <div class="box">
+                  <p class="subtitle">Discussions</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+                </div>
+              </div>
+              <div class="column is-half-desktop">
+                <div class="box">
+                  <p class="subtitle">Events</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+                </div>
+              </div>
+            </div>
+            <div class="columns">
+              <div class="column is-half-desktop">
+                <div class="box">
+                  <p class="subtitle">Study Resources</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+                </div>
+              </div>
+              <div class="column is-half-desktop">
+                <div class="box">
+                  <p class="subtitle">Study Groups</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <h1 class="title has-text-centered">Login</h1>
+            <div class="login-form is-flex is-flex-wrap-wrap">
+              <form @submit.prevent="login" class="box">
+                <div class="field">
+                  <label for="email" class="label">Username</label>
+                  <input type="text" id="email" name="username" v-model="username" class="input" required>
+                </div>
+                <div class="field">
+                  <label for="password" class="label">Password</label>
+                  <input type="password" id="password" name="password" v-model="password" class="input" required>
+                </div>
+                <div class="field">
+                  <button id="submit-button" type="submit" class="button is-success is-fullwidth">Login</button>
+                </div>
+                <div class="field">
+                  <RouterLink to="/forgot-password">Forgot Password?</RouterLink>
+                </div>
+                <div class="field">
+                  <span>Don't have an account?</span>
+                  <RouterLink to="/register" class="button is-link is-outlined is-small ml-2">Register</RouterLink>
+                </div>
+                <p v-if="sessionExpired" class="help is-danger">Your session has expired, please log in again.</p>
+                <p v-if="incorrectAuth" class="help is-danger">Incorrect username and/or password entered. Please try again.</p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+
+  <script>
+  export default {
+    name: 'HomeView',
+    data() {
+      return {
+        username: '',
+        password: '',
+        incorrectAuth: false,
+        sessionExpired: false,
+      };
+    },
+    mounted() {
+      if (this.$route.query.sessionExpired) {
+        this.sessionExpired = true;
+      }
+    },
+    methods: {
+      login() {
+        this.$store.dispatch('userLogin', {
+          username: this.username,
+          password: this.password,
+        })
+        .then(() => {
+          this.$router.push({ name: 'Dashboard' });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.incorrectAuth = true;
+        });
+      },
+    },
+  };
+  </script>
+
+  
+<style scoped>
+    form {
+        width: 500px;
+        margin: 19px auto;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+        padding: 40px;
+    }
+</style>
--- front-end/src/views/RegisterForm.vue
+++ front-end/src/views/RegisterForm.vue
@@ -1,35 +1,72 @@
 <template>
-    <div>
-        <h1>Account Registration</h1>
-        <form @submit.prevent="registerUser" method="post">
-            <label for="first-name">First name:</label>
-            <input type="text" name="first_name" id="first-name" v-model="firstName" required>
-            <label for="surname">Surname:</label>
-            <input type="text" name="surname" id="surname" v-model="surname" required>
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username" v-model="username" required>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" v-model="email" required>
-            <label for="password1">Password:</label>
-            <input type="password" name="password1" id="password1" v-model="password1" required>
-            <label for="password2">Confirm Password:</label>
-            <input type="password" name="password2" id="password2" v-model="password2" required>
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-two-thirds-mobile is-half-tablet is-one-third-desktop">
+          <h1 class="title has-text-centered">Account Registration</h1>
+          <form @submit.prevent="registerUser" method="post">
             <div class="field">
-                <div class="control">
-                    <label class="checkbox">
-                        <input type="checkbox" name="privacy_policy" v-model="privacyPolicy" required>
-                        I agree to the <a href="/privacy_policy">privacy policy</a>.
-                    </label>
-                </div>
+              <label class="label" for="first-name">First name:</label>
+              <div class="control">
+                <input class="input" type="text" name="first_name" id="first-name" v-model="firstName" required>
+              </div>
             </div>
-            <button id="register-button" type="submit">Register</button>
-        </form>
+            <div class="field">
+              <label class="label" for="surname">Surname:</label>
+              <div class="control">
+                <input class="input" type="text" name="surname" id="surname" v-model="surname" required>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="username">Username:</label>
+              <div class="control">
+                <input class="input" type="text" name="username" id="username" v-model="username" required>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="email">Email:</label>
+              <div class="control">
+                <input class="input" type="email" name="email" id="email" v-model="email" required>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="password1">Password:</label>
+              <div class="control">
+                <input class="input" type="password" name="password1" id="password1" v-model="password1" required>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="password2">Confirm Password:</label>
+              <div class="control">
+                <input class="input" type="password" name="password2" id="password2" v-model="password2" required>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <label class="checkbox">
+                  <input type="checkbox" name="privacy_policy" v-model="privacyPolicy" required>
+                  I agree to the <a href="/privacy_policy">privacy policy</a>.
+                </label>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control has-text-centered">
+                <button class="button is-primary" type="submit">Register</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-</template>
-
-<style>
-@import url('./../assets/registration.css');
-</style>
+  </template>
+  
+  <style scoped>
+  @media screen and (max-width: 768px) {
+    .column.is-two-thirds-mobile {
+      width: 100%;
+    }
+  }
+  </style>
+  
 
 <script>
 // import axios from 'axios';
@@ -39,12 +76,12 @@ export default {
     name: 'RegisterForm',
     data() {
         return {
-            firstName: 'asdf',
-            surname: 'asdf',
-            username: 'asdf',
-            email: 'asdf@asdf.com',
-            password1: 'asdf',
-            password2: 'asdf',
+            firstName: '',
+            surname: '',
+            username: '',
+            email: '',
+            password1: '',
+            password2: '',
         };
     },
     methods: {
@@ -78,3 +115,4 @@ export default {
     },
 };
 </script>
+
